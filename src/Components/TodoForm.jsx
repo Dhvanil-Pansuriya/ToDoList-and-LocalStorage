1c5c6a805eaf6d88b06c354b98abc538a2e2e7ab
Dhvanil Pansuriya <dhvanilpansuriya2005@gmail.com>
2024-01-08 22:10:53 +0530

    Todo ( LocalStorage & Hooks )

---
+++ src/Components/TodoForm.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { useTodo } from '../context';
+
+function TodoForm() {
+
+    const [todo, setTodo] = useState("");
+    const { addTodo } = useTodo();
+
+    const add = (e) => {
+        e.preventDefault
+        if (!todo) {
+            return
+        } else {
+            addTodo({ todo, completed: false })
+            setTodo("")
+        }
+    }
+
+    return (
+        <form className="flex" onSubmit={add}>
+            <input
+                type="text"
+                placeholder="Write Todo..."
+                className="w-full border text-[#464646] border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+            />
+            <button type="submit" className="rounded-r-lg px-3 py-1 bg-[#858585] text-white shrink-0">
+                Add
+            </button>
+        </form>
+    );
+}
+
+export default TodoForm;
+
